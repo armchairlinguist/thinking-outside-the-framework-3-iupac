91b24d518dc7732f72f8c195c4a4463bcd1896c8
Markus Roberts <Markus@reality.com>
2014-08-05 11:31:05 -0700

    AOP refactor

--- iupac.rb
+++ iupac.rb
@@ -1,3 +1,6 @@
+#
+# The basic data objects
+#
 class Element
   attr_reader :name,:symbol,:mass,:valence,:skeletal
   def initialize(n,s,m,v,sk=false)
@@ -7,15 +10,6 @@ class Element
     @valence = v
     @skeletal = sk
     Object.const_set(s,self)
-  end
-  def atoms
-    [Atom.new(self)]
-  end
-  def [](*neighbors)
-    Moiety.new(self,*neighbors) { |m,i,x| m.atoms[0].bond(x[0]) if i > 0 }
-  end
-  def to_s
-    symbol
   end
 end
 
@@ -37,27 +31,18 @@ class Atom
     other.bonds << self
     self
   end
-  def atoms
-    [self]
-  end
-  def to_s
-    element.to_s
-  end
 end
 
 class Moiety
   attr_reader :atoms
-  def initialize(*moieties,&join)
-    parts = moieties.map(&:atoms)
-    @atoms = parts.flatten
-    parts.each_with_index { |x,i| join[self,i,x] }
-  end
+end
+
+#
+# A few simple examples of how we might compute with them
+#
+class Moiety
   def mass
     atoms.map { |a| a.element.mass }.inject(&:+)
-  end
-  def hydrated
-    atoms.each { |a| atoms << H[a].atoms[0] while a.bonds.count < a.element.valence }
-    self
   end
   def emperical_formula
     atoms.
@@ -66,20 +51,35 @@ class Moiety
       sort.
       join
   end
-  def dot(file_name)
-    File.open(file_name,'w') { |f|
-      f.puts 'graph "" {'
-      index = {}
-      atoms.
-        flat_map { |a| [a,*a.bonds] }.
-        uniq.
-        each_with_index { |a,i|
-          index[a] = i
-          f.puts "    a#{i} [label=#{a.element}];"
-          a.bonds.each { |b| f.puts "      a#{i} -- a#{index[b]};" if index[b] }
-        }
-      f.puts '}'
-    }
+end
+
+#
+# A little DSL to let us construct interesting examples
+#
+class Element
+  def atoms
+    [Atom.new(self)]
+  end
+  def [](*neighbors)
+    Moiety.new(self,*neighbors) { |m,i,x| m.atoms[0].bond(x[0]) if i > 0 }
+  end
+end
+
+class Atom
+  def atoms
+    [self]
+  end
+end
+
+class Moiety
+  def initialize(*moieties,&join)
+    parts = moieties.map(&:atoms)
+    @atoms = parts.flatten
+    parts.each_with_index { |x,i| join[self,i,x] }
+  end
+  def hydrated
+    atoms.each { |a| atoms << H[a].atoms[0] while a.bonds.count < a.element.valence }
+    self
   end
 end
 
@@ -105,14 +105,57 @@ class Fixnum
   end
 end
 
-p O[H,H].mass
-p O[H,H].emperical_formula
+#
+# Some pretty printing suport
+#
+class Element
+  def to_s
+    symbol
+  end
+end
+
+class Atom
+  def to_s
+    element.to_s
+  end
+end
+
+class Moiety
+  def dot(file_name)
+    File.open(file_name,'w') { |f|
+      f.puts 'graph "" {'
+      index = {}
+      atoms.
+        flat_map { |a| [a,*a.bonds] }.
+        uniq.
+        each_with_index { |a,i|
+          index[a] = i
+          f.puts "    a#{i} [label=#{a.element}];"
+          a.bonds.each { |b| f.puts "      a#{i} -- a#{index[b]};" if index[b] }
+        }
+      f.puts '}'
+    }
+  end
+end
+
+#
+# A few examples / test cases
+#
+
+water = O[H,H]
+p water.mass
+p water.emperical_formula
 
 ethane = C[H,H,H,C[H,H,H]]
 p ethane.mass
 p ethane.emperical_formula
 ethane.dot('ethane.dot')
 
-chain(*8.of {C}).hydrated.dot('octane.dot')
-ring(*6.of {C}).hydrated.dot('cyclohexane.dot')
-ring(*6.of {C}) {|m,i,x,l| l.bond(x[0]) if i.odd? }.hydrated.dot('benzene.dot')
+octane = chain(*8.of {C}).hydrated
+octane.dot('octane.dot')
+
+cyclohexane = ring(*6.of {C}).hydrated
+cyclohexane.dot('cyclohexane.dot')
+
+benzene = ring(*6.of {C}) {|m,i,x,l| l.bond(x[0]) if i.odd? }.hydrated
+benzene.dot('benzene.dot')
